9475a6ffd46628e1709f5862f2fc5bdba046d5cc
aurelien-gallea <aurelien.gallea@laplateforme.io>
2025-04-19 10:47:57 +0200

    [chore] add form pages, header and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { FormWithSignalsComponent } from './features/form-with-signals/form-with-signals.component';
+import { FormWithObservablesComponent } from './features/form-with-observables/form-with-observables.component';
+import { HomePageComponent } from './features/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: "Form with Signals",
+        path: "form-with-signals",
+        component: FormWithSignalsComponent
+    },
+    {
+        title: "Form with Observables",
+        path: "form-with-observables",
+        component: FormWithObservablesComponent
+    },
+    {
+        title: "Home",
+        path: "",
+        component: HomePageComponent
+    }
+];
